d3da95f0292b271fcdea4f9a20e2b2b82a2eb97f
veronika <catshiu@bk.ru>
2018-09-17 13:11:44 +0300

    коммент

--- vorobieva.vb/notesvb.c
+++ vorobieva.vb/notesvb.c
@@ -7,7 +7,7 @@
 
 int main() {
         for (int i = 12; i<121; i+=12) {
-                printf("%ld, %ld\n", -ipoteka(i)+6000000, vklad(i)-arenda(i));
+                printf("%ld, %ld\n", -ipoteka(i)+6000000, vklad(i)-arenda(i)); //коммент
         }
 
         return 0;
