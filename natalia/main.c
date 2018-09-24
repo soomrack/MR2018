356c041958297f65bab8bce9bcfd7b6d85b7d39d
Natalia <42980988+MaltsevaNata@users.noreply.github.com>
2018-09-24 19:44:53 +0300

    Merge branch 'master' of C:\Users\natas\CLionProjects\Common with conflicts.

--- natalia/main.c
+++ natalia/main.c
@@ -1,35 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+
+
+/*
 #include <sys/types.h>
-#include <winsock2.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <err.h>
+*/
 #include <windows.h>
-
+#include <winsock2.h>
 #include <ws2tcpip.h>
-#include <errno.h>
-
+// link with Ws2_32.lib
+#pragma comment(lib,"Ws2_32.lib")
+char chislo[5];
+int count;
 char response[] = "HTTP/1.1 200 OK\r\n"
 "Content-Type: text/html; charset=UTF-8\r\n\r\n"
 "<!DOCTYPE html><html><head><title>Hello World!</title></head>"
-"<body><h1>Hello World!</h1></body></html>\r\n";
+"<body><h1>Hello World!</h1> </body></html>\r\n";
 
 int main()
 {
-  int one = 1 ;
-  int client_fd;
+    WSADATA wsdata;
+    WSAStartup(0x0101,&wsdata);
+    int one = 1, client_fd;
   struct sockaddr_in svr_addr, cli_addr;
   socklen_t sin_len = sizeof(cli_addr);
 
   int sock = socket(AF_INET, SOCK_STREAM, 0);
   if (sock < 0)
     printf("can't open socket");
-  setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(int) );
 
-// привязать сокет к устройству
-  int port = 8080;
+  setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(int));
+
+  int port = 8082;
   svr_addr.sin_family = AF_INET;
   svr_addr.sin_addr.s_addr = INADDR_ANY;
   svr_addr.sin_port = htons(port);
+
+  count = 0;
 
   if (bind(sock, (struct sockaddr *) &svr_addr, sizeof(svr_addr)) == -1) {
     close(sock);
@@ -39,14 +52,21 @@ int main()
   listen(sock, 5);
   while (1) {
     client_fd = accept(sock, (struct sockaddr *) &cli_addr, &sin_len);
+
     printf("got connection\n");
+    count+=1;
+    printf("%d\n",count);
+    sprintf(chislo, "%d", count);
 
     if (client_fd == -1) {
       perror("Can't accept");
       continue;
     }
 
-    write(client_fd, response, sizeof(response) - 1); /*-1:'\0'*/
+
+    send(client_fd, response, sizeof(response) - 1,0);
+    send(client_fd, &chislo, 1,0);
     close(client_fd);
   }
+    WSACleanup();
 }
