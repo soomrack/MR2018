70cd2e20279431dfa5ebe6e1c4b5cf549704c12c
ElizavetaTs <43005863+ElizavetaTs@users.noreply.github.com>
2018-10-15 13:31:51 +0300

    Умножение матриц + тест

--- Elizaveta/Elib.c
+++ Elizaveta/Elib.c
@@ -2,4 +2,31 @@
 // Created by Елизавета on 17.09.2018.
 //
 
+
+#include <stdio.h>
+#include <stdlib.h>
 #include "Elib.h"
+
+
+
+void matrix_multiplication (int* A, int* B, int* C, int m, int n, int q)
+{
+for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < q; j++)
+        {
+            C[i*q+j] = 0;
+            for (int k = 0; k < n; k++)
+            {
+                C[i*q+j] += A[i*n+k] * B[k*q+j];
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
