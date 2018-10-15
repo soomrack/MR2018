aa8fb0395d4209e98f2c4cdf5bdf8f67b927a843
NenahovIvan <v.271998@yandex.ru>
2018-10-15 15:26:35 +0300

    след и вывод2

--- Nenahov/Matrixxxxxxxx/Matrixbiblio.c
+++ Nenahov/Matrixxxxxxxx/Matrixbiblio.c
@@ -4,6 +4,8 @@
 
 #include "Matrixbiblio.h"
 #include <stdio.h>
+#include<stdlib.h>
+
 
 double matrix_trace(const Matrix A)
 {
@@ -26,4 +28,20 @@ void matrix_print(const Matrix A)
         }
         printf("\n");
     }
+}
+
+Matrix matrix_sum(const Matrix A, const Matrix B)
+{
+    Matrix C;
+    C.rows=A.rows;
+    C.cols=A.cols;
+    C.data=malloc(C.cols*C.rows*sizeof(double));
+    for(int t=0;t<A.rows;t++)
+    {
+        for(int i=0;i<A.rows;i++)
+        {
+            C.data[i+t*A.rows]=A.data[i+t*A.rows]+B.data[i+t*A.rows];
+        }
+    }
+    return C;
 }
--- Nenahov/Matrixxxxxxxx/main.c
+++ Nenahov/Matrixxxxxxxx/main.c
@@ -2,12 +2,48 @@
 #include<stdlib.h>
 #include<time.h>
 #include "Matrixbiblio.h"
+#include<math.h>
 /*
 typedef struct {
     unsigned int rows;
     unsigned int cols;
     double* data;
 } Matrix;*/
+
+double Minor(Matrix A,int n)
+{
+    if(A.rows>2)
+    {
+        A.rows--;
+        for(int t=0;t<A.rows;t++)
+        {
+            for(int i=0;i<A.rows;i++)
+            {
+                if(i>=n)
+                {
+                    A.data[i+t*A.rows]=A.rows[i+t*A.rows+1];
+                    
+                }
+
+            }
+        }
+    }
+}
+double matrix_determinant(const Matrix A)
+{
+    double det=0;
+    Matrix M;
+    M.rows=A.rows-1;
+    M.cols=A.cols-1;
+    M.data=malloc(M.cols*sizeof(double));
+    for(int t=0;)
+    for(int i=0;i<A.rows;i++)
+    {
+        M.data[i]=
+        det+=pow(-1,1+i+1)*A.data[1+i*A.rows]*M.data[1+i*M.rows];
+    }
+}
+
 
 
 int main() {
@@ -25,13 +61,29 @@ int main() {
 
     for(int t=0;t<A.rows;t++)
     {
-        for(int i=0;i<A.rows;i++)
+        for(int i=0;i<A.cols;i++)
         {
           A.data[i+t*A.rows]=rand();
         }
     }
-    matrix_print(A);
-    printf("trace=%4.2lf",matrix_trace(A));
+    Matrix B;
 
+    B.rows=3;
+    B.cols=3;
+    B.data=malloc(A.cols*A.rows*sizeof(double));
+    for(int t=0;t<A.rows;t++)
+    {
+        for(int i=0;i<A.rows;i++)
+        {
+            B.data[i+t*A.rows]=rand();
+        }
+    }
+    matrix_sum(A,B);
+
+    matrix_print(A);
+    printf("trace=%4.2lf\n",matrix_trace(A));
+    matrix_print(B);
+    printf("\n");
+    matrix_print(matrix_sum(A,B));
     return 0;
 }
