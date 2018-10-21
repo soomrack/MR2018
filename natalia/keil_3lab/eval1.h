694b7e882d566149c7e97cc974adb6e913d73f14
MaltsevaNata <you@example.com>
2018-10-21 14:34:13 +0300

    3 laba v keile

---
+++ natalia/keil_3lab/eval1.h
@@ -0,0 +1,5 @@
+#include <stdint.h>
+
+int32_t eval1( const char * formula );
+
+int64_t eval_64( const char * formula );
---
+++ natalia/keil_3lab/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "eval1.h"
+#include <string.h>
+#include <stdlib.h>
+
+
+int main(void)
+{
+    printf("Otvet %d \n",eval1(" 10-(-100)  ")); // returns 3
+
+    return 0;
+}
