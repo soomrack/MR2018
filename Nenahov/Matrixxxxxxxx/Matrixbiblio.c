f46dea114bb738e0c653200e3a85d8d8398170e4
NenahovIvan <v.271998@yandex.ru>
2018-10-22 12:02:08 +0300

    determinant+trans+something else

--- Nenahov/Matrixxxxxxxx/Matrixbiblio.c
+++ Nenahov/Matrixxxxxxxx/Matrixbiblio.c
@@ -24,16 +24,32 @@ extern Matrix matrix_rand(const unsigned int rows, const unsigned int cols)
     return A;
 }
 
+extern Matrix matrix_trans(const Matrix A)
+{
+    Matrix B;
+    B.rows=A.cols;
+    B.cols=A.rows;
+    for(int t=0;t<B.rows;t++)
+    {
+        for(int i=0;i<B.cols;i++)
+        {
+            B.data[t+i*B.cols]=A.data[i+t*A.cols];
+        }
+    }
+    return B;
+}
+
 extern Matrix matrix_mult__scalar(const double scalar, const Matrix A)
 {
+    Matrix B;
     for(int t=0;t<A.rows;t++)
     {
         for(int i=0;i<A.cols;i++)
         {
-            A.data[t+i*A.cols]=scalar*A.data[t+i*A.cols];
+            B.data[t+i*A.cols]=scalar*A.data[t+i*A.cols];
         }
     }
-    return A;
+    return B;
 
 }
 
@@ -54,7 +70,7 @@ void matrix_print(const Matrix A)
     {
         for(int i=0;i<A.rows;i++)
         {
-            printf("%10.2lf\t",A.data[i*A.rows+t]);
+            printf("%10.2lf\t",A.data[t*A.cols+i]);
         }
         printf("\n");
     }
@@ -70,7 +86,7 @@ Matrix matrix_sum(const Matrix A, const Matrix B)
     {
         for(int i=0;i<A.rows;i++)
         {
-            C.data[i+t*A.rows]=A.data[i+t*A.rows]+B.data[i+t*A.rows];
+            C.data[i+t*A.cols]=A.data[i+t*A.cols]+B.data[i+t*A.cols];
         }
     }
     return C;
@@ -108,3 +124,55 @@ extern Matrix matrix_mult(const Matrix A, const Matrix B)
     }
     return C;
 }
+
+Matrix Minor(Matrix A,int n)
+{
+    Matrix B;
+    B.rows=A.rows-1;
+    B.cols=A.cols-1;
+    for(int rows=0;rows<B.rows;rows++)
+    {
+        for(int cols=0;cols<B.cols;cols++)
+        {
+            if(cols<n)
+            {
+                B.data[cols+rows*B.cols]=A.data[cols+(rows+1)*A.cols];
+            }
+            else
+            {
+                B.data[cols + rows * B.cols] = A.data[cols+1+(rows+1)*A.cols];
+            }
+
+        }
+    }
+    return B;
+}
+double matrix_determinant(const Matrix A)
+{
+    double det=0;
+    if(A.cols==1)
+    {
+        det=A.data[0];
+    }
+    if(A.cols==2)
+    {
+        det=A.data[0]*A.data[3]-A.data[2]*A.data[1];
+    }
+    else{
+        int k=1;
+        for(int rows=0;rows<A.rows;rows++)
+        {
+            det+=k*A.data[rows]*matrix_determinant(Minor(A,rows));
+            k*=(-1);
+        }
+    }
+    return det;
+}
+
+extern Matrix matrix_invert(const Matrix A)
+{
+    Matrix B;
+    B=matrix_mult__scalar(1/matrix_determinant(A),matrix_trans(A));
+    return B;
+}
+
