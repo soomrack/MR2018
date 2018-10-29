8571ab3610c82e13ea97c55116b47cd20cac7250
KhalyavinNikita <nikitahalyavin5@gmail.com>
2018-10-29 14:55:14 +0300

    Много в матрицах

--- NikitaKhalyavin/CPPtest/MatrLib.cpp
+++ NikitaKhalyavin/CPPtest/MatrLib.cpp
@@ -5,3 +5,8 @@
 #include "MatrLib.h"
 
 
+unsigned long long int fact(unsigned int n){
+    unsigned long long int out = 1;
+    for(int i = 2; i < n; i++)out*=i;
+    return  out;
+}
--- NikitaKhalyavin/CPPtest/MatrLib.h
+++ NikitaKhalyavin/CPPtest/MatrLib.h
@@ -12,12 +12,20 @@
 #endif //MR2018_MATRLIB_H
 
 
+static unsigned long long int fact(unsigned int n);
+
+
 class Matrix{
 private:
     int m_cols;
     int m_rows;
 public:
     double * data;
+
+    Matrix(){
+
+    }
+
     Matrix(int cols, int rows):    m_cols(cols), m_rows(rows){
         data = (double*)malloc(m_cols * m_rows * sizeof(double));
         srand(time(NULL));
@@ -55,14 +63,107 @@ public:
     double Trace(){
         double trace = 0;
 
-        for(int i = 0; i < m_rows; i++){     //construction sub-matrix
+        for(int i = 0; i < m_rows; i++){
             trace += data[i + ( i * m_cols)];
         }
         return trace;
     }
 
+    Matrix Trans(){
+        Matrix out(m_rows, m_cols);
+        for(int i = 0; i < m_rows; i++){     //construction sub-matrix
+            for(int j = 0; j < m_cols; j++){
+                out.data[j + ( i * m_cols)] = data[i + ( j * m_rows)];
+            }
+        }
+        return out;
+    }
 
+    Matrix operator + (const Matrix& Arg){
+        Matrix out(m_cols, m_rows);
+        for(int i = 0; i < m_cols; i++){     //construction sub-matrix
+            for(int j = 0; j < m_rows; j++){
+                out.data[j + ( i * m_rows)] = data[j + ( i * m_rows)] + Arg.data[j + ( i * m_rows)];
+            }
+        }
+        return out;
+    }
+
+    Matrix operator * (const Matrix& Arg){
+        Matrix out(Arg.m_cols, m_rows);
+        for(int i = 0; i < out.m_rows; i++){
+            for(int j = 0; j < out.m_cols; j++){
+                out.data[i * out.m_cols + j] = 0;
+                for(int k = 0; k < m_cols; k++) {
+                    out.data[i * out.m_cols + j] += data[i * m_cols + k] * Arg.data[k * Arg.m_cols + j];
+                }
+            }
+        }
+        return out;
+    }
+
+    Matrix operator * (double Arg){
+        Matrix out (m_cols, m_rows);
+        for(int i = 0; i < m_rows; i++){     //construction sub-matrix
+            for(int j = 0; j < m_cols; j++){
+                out.data[j + ( i * m_cols)] = data[j + ( i * m_cols)] * Arg;
+            }
+        }
+        return out;
+    }
+
+    void operator = (const Matrix& Arg){
+        free(data);
+        m_cols = Arg.m_cols;
+        m_rows = Arg.m_rows;
+        data = (double*)malloc(m_cols * m_rows * sizeof(double));
+        for(int i = 0; i < m_rows; i++){
+            for(int j = 0; j < m_cols; j++){
+                data[j + ( i * m_cols)] = Arg.data[j + ( i * m_cols)];
+            }
+        }
+    }
+
+    Matrix Power (int power){
+        Matrix C(m_cols, m_rows, 1);
+        Matrix B(m_cols, m_rows);
+        for(int p = 0; p < power; p++) {
+            B = C;
+            C = (B * *this);
+        }
+        free(B.data);
+        return C;
+    }
+
+    Matrix Exp(){
+        Matrix C(m_cols, m_rows, 0);
+        Matrix B;
+        for(unsigned int k = 1; k < 1000; k++) {
+            int flag = 1;
+            B = this->Power(k);
+            unsigned long long int f = fact(k);
+            for (int i = 0; i < C.m_rows; i++) {
+                for (int j = 0; j < C.m_cols; j++) {
+                    C.data[j + (i * C.m_cols)] += B.data[j + (i * B.m_cols)] / f;
+                    flag &=  !( ( B.data[j + (i * B.m_cols)] / f / C.data[j + (i * C.m_cols)] ) > 0.0001);
+                }
+            }
+            if(flag) break;
+        }
+        free(B.data);
+        return C;
+    }
 };
+
+
+
+
+
+
+
+
+
+
 
 class Polinom{
 private:
@@ -128,7 +229,7 @@ public:
         printf("\n");
     }
 
-    Polinom operator * (double& Arg){
+    Polinom operator * (double Arg){
         Polinom out(maxPower);
         for(int p1 = 0; p1 < maxPower; p1++){
             out.data[p1] = data[p1] * Arg;
