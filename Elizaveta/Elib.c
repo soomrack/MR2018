cc22da5e07e2537f0bafc92e9f965ed610692577
ElizavetaTs <43005863+ElizavetaTs@users.noreply.github.com>
2018-11-11 14:35:29 +0300

    матрицы си

--- Elizaveta/Elib.c
+++ Elizaveta/Elib.c
@@ -9,7 +9,6 @@
 #include <Math.h>
 
 
-
 void matrix_multiplication (int* A, int* B, int* C, int m, int n, int q)
 {
     for (int i = 0; i < m; i++)
@@ -25,7 +24,6 @@ void matrix_multiplication (int* A, int* B, int* C, int m, int n, int q)
         }
 }
 
-
 // След матрицы
 extern double matrix_trace(const Matrix A)
 {
@@ -37,41 +35,277 @@ extern double matrix_trace(const Matrix A)
     return s;
 }
 
+// Миноры
+extern Matrix matrix_minor(int row, int col, const Matrix A)
+{
+    Matrix result = {A.rows-1, A.cols-1};
+    result.data = malloc(result.rows * result.cols * sizeof(double));
+    int k = 0;
+    for (int i = 0; i < A.rows; i++)
+    {
+        for (int j = 0; j < A.cols; j++)
+        {
+            if ((i == row) || (j == col))
+            {
+                continue;
+            }
+            result.data[k] = A.data[i * A.cols + j];
+            k++;
+        }
+    }
+    return result;
+}
+
+// Алгебраические дополнения
+extern Matrix matrix_dop(const Matrix A)
+{
+    Matrix result = {A.rows, A.cols};
+    result.data = malloc(result.rows * result.cols * sizeof(double));
+    Matrix M;
+    for (int i = 0; i < A.rows; i++)
+    {
+        for (int j = 0; j < A.cols; j++)
+        {
+            M = matrix_minor(i, j, A);
+            result.data[i*A.cols + j] = pow((-1), i + j) * matrix_determinant(M);
+            free(M.data);
+        }
+    }
+    return result;
+}
 
 // Определитель матрицы
 extern double matrix_determinant(const Matrix A)
 {
-    double D, Ad, m;
-    double M[4];
-    int j;
-
+    double D = 0;
     switch (A.rows)
         {
             case 1:
                 return A.data[0];
             case 2:
                 return (A.data[0] * A.data[3] - A.data[1] * A.data[2]);
-            case 3:
+            default:
             {
-                for (int i = 0; i < A.rows; i++)
+                Matrix dop = matrix_dop(A);
+                for (int j = 0; j < A.cols; j++)
                 {
-                    j = 0;
-                    for (int  k = 0; k < (A.rows * A.cols); k++)
-                    {
-                        if ((k < (i * A.cols)) && (k > (i * A.cols + 2)))
-                        {
-                            M[j] = A.data[k];
-                            j += 1;
-                        }
-
-                    }
-                    m = M[0] * M[3] - M[1] * M[2];
-                    Ad = pow((-1), i + 1) * m;
-                    D += A.data[i * A.cols] * Ad;
+                    D += A.data[j] * dop.data[j];
                 }
                 return D;
             }
         }
 }
+
+// Собственные числа матрицы
+extern Matrix matrix_eigen_values(const Matrix A);
+
+
+// Матрица собственных векторов
+extern Matrix matrix_eigen_vectors(const Matrix A);
+
+
+// Сумма матриц
+extern Matrix matrix_sum(const Matrix A, const Matrix B)
+{
+    if ((A.rows != B.rows) || (A.cols != B.cols))
+    {
+        printf("Sum error - different size\n");
+    }
+    else
+    {
+        Matrix result = {A.rows, A.cols};
+        result.data = malloc(result.rows * result.cols * sizeof(double));
+        for (int i = 0; i < A.rows; i++)
+        {
+            for (int j = 0; j < A.cols; j++)
+            {
+                result.data[i*A.cols+j] = A.data[i*A.cols+j] + B.data[i*A.cols+j];
+            }
+
+        }
+
+        return result;
+    }
+}
+
+
+// Умножение матриц
+extern Matrix matrix_mult(const Matrix A, const Matrix B)
+{
+    if (A.cols != B.rows)
+    {
+        printf ("Multiplication error \n");
+    }
+    else
+    {
+        Matrix result = {A.rows, B.cols};
+        result.data = malloc(result.rows * result.cols * sizeof(double));
+        for (int i = 0; i < A.rows; i++)
+        {
+            for (int j = 0; j < B.cols; j++)
+            {
+                result.data[i * B.cols + j] = 0;
+                for (int k = 0; k < A.cols; k++) {
+                    result.data[i * B.cols + j] += A.data[i * A.cols + k] * B.data[k * B.cols + j];
+                }
+            }
+        }
+
+        return result;
+    }
+}
+
+
+// Умножение матрицы на скаляр
+extern Matrix matrix_mult__scalar(const double scalar, const Matrix A)
+{
+    Matrix result = {A.rows, A.cols};
+    result.data = malloc(result.rows * result.cols * sizeof(double));
+    for (int i = 0; i < A.rows; i++)
+    {
+        for (int j = 0; j < A.cols; j++)
+        {
+             result.data[i * A.cols + j] = scalar * A.data[i * A.cols + j];
+        }
+    }
+
+    return result;
+}
+
+// Транспонирование матрицы
+extern Matrix matrix_trans(const Matrix A)
+{
+    Matrix result = {A.cols, A.rows};
+    result.data = malloc(result.rows * result.cols * sizeof(double));
+    for (int i = 0; i < A.rows; i++)
+    {
+        for (int j = 0; j < A.cols; j++)
+        {
+            result.data[j * A.rows + i] = A.data[i * A.cols + j];
+        }
+    }
+
+    return result;
+}
+
+// Обращение матрицы
+extern Matrix matrix_invert(const Matrix A)
+{
+    double det = matrix_determinant(A);
+    if (det == 0)
+    {
+        printf ("No invert matrix \n");
+    }
+    else
+    {
+        Matrix result;
+        result = matrix_mult__scalar(1/det, matrix_trans(matrix_dop(A)));
+        return result;
+    }
+}
+// Матричная экспонента
+extern Matrix matrix_exp(const Matrix A);
+
+// Возведение матрицы в степень (натуральное число или 0)
+extern Matrix matrix_power(const Matrix A, const unsigned int power)
+{
+    if (power < 2)
+    {
+        printf("wrong power \n");
+    }
+    else
+    {
+        if (A.rows != A.cols)
+        {
+            printf("only square matrix \n");
+        }
+        else
+        {
+            Matrix result = A;
+            for (int i = 0; i < (power-1); i++)
+            {
+                result = matrix_mult(result, A);
+            }
+
+        return result;
+        }
+    }
+}
+
+// Единичная матрица
+extern Matrix matrix_one(const unsigned int size)
+{
+    Matrix result = {size, size};
+    result.data = malloc(result.rows * result.cols * sizeof(double));
+    for (int i = 0; i < result.rows; i++)
+    {
+        for (int j = 0; j < result.cols; j++)
+            {
+                if (i == j)
+                {
+                    result.data[i * result.cols + j] = 1;
+                }
+                else
+                {
+                    result.data[i * result.cols + j] = 0;
+                }
+            }
+
+    }
+
+    return result;
+}
+
+// Нулевая матрица
+extern Matrix matrix_zero(const unsigned int rows, const unsigned int cols)
+{
+    Matrix result = {rows, cols};
+    result.data = malloc(result.rows * result.cols * sizeof(double));
+    for (int i = 0; i < result.rows; i++)
+    {
+        for (int j = 0; j < result.cols; j++)
+        {
+            result.data[i * result.cols + j] = 0;
+        }
+    }
+
+    return result;
+}
+
+// Матрица оставленная из случайных чисел из диапазона [-1, +1]
+extern Matrix matrix_rand(const unsigned int rows, const unsigned int cols)
+{
+    Matrix result = {rows, cols};
+    result.data = malloc(result.rows * result.cols * sizeof(double));
+    for (int i = 0; i < result.rows; i++)
+    {
+        for (int j = 0; j < result.cols; j++)
+        {
+            result.data[i * result.cols + j] = -1 + rand()%3;
+        }
+    }
+
+    return result;
+}
+
+// Вывести матрицу на экран
+extern void matrix_print(const Matrix A)
+{
+    printf("matrix \n");
+    for (int i = 0; i < (A.cols * A.rows); i++)
+    {
+        printf ("%5.2lf     ", A.data[i]);
+        if ((i+1)%A.cols == 0)
+        {
+            printf("\n");
+        }
+    }
+}
+
+// ТАУ
+
+// Решение уравнения Ляпунова X * A + A^T * X = -C
+extern Matrix matrix_lyapunov_equation(const Matrix A, const Matrix C);
+
 
 
---
+++ Elizaveta/tests/new.c
@@ -0,0 +1,72 @@
+//
+// Created by Елизавета on 15.10.2018.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "../Elib.h"
+
+#define M 2
+#define N 3
+#define Q 4
+
+
+int main ()
+{
+    double arqA[N][N] = {{0, 1, 2},
+                         {1, 2, 3},
+                         {2, 3, 4}};
+
+    double arqB[N][N] = {{1, 2, 3},
+                         {2, 3, 4},
+                         {3, 4, 8}};
+
+    Matrix A = {N, N, *arqA};
+    Matrix B = {N, N, *arqB};
+    Matrix Sum;
+    Matrix Mult;
+    Matrix MultScal;
+    Matrix Transp;
+    Matrix Minor;
+    Matrix Dop;
+    Matrix Invert;
+    Matrix power;
+
+    double trace, det;
+
+    trace = matrix_trace(A);
+    printf("trace \n %lf \n", trace);
+
+    Minor = matrix_minor(1, 1, B);
+    matrix_print(Minor);
+
+    Dop = matrix_dop(B);
+    matrix_print(Dop);
+
+    det = matrix_determinant(B);
+    printf("determinant \n %lf \n", det);
+
+    Sum = matrix_sum(A, B);
+    matrix_print(Sum);
+
+    Transp = matrix_trans(A);
+    matrix_print(Transp);
+
+    MultScal = matrix_mult__scalar(5, A);
+    matrix_print(MultScal);
+
+    Mult = matrix_mult(A, B);
+    matrix_print(Mult);
+
+    Invert = matrix_invert(B);
+    matrix_print(Invert);
+
+    matrix_print(matrix_one(4));
+    matrix_print(matrix_zero(4, 3));
+    matrix_print(matrix_rand(3, 4));
+
+    power = matrix_power(B, 7);
+    matrix_print(power);
+
+    return 0;
+}
