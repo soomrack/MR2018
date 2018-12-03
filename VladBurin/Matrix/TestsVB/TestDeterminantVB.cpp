f179124bf1db7cf8bb2a6b345ae7c3bb3f2800f9
Vlad Burin <bvv98@yandex.ru>
2018-12-03 11:06:36 +0200

    Сделал на cpp

---
+++ VladBurin/Matrix/TestsVB/TestDeterminantVB.cpp
@@ -0,0 +1,75 @@
+//
+// Created by vladburin on 30.11.18.
+//
+#include <stdio.h>
+#include <math.h>
+#include "../matrix.h"
+#include <iostream>
+#include <stdlib.h>
+
+#define STRINGIFY( x ) #x
+
+#define TEST_EQUAL( matrix, expected )   \
+{                                                                           \
+    cout << endl << "Matrix Determinant "  STRINGIFY(matrix) "         ";               \
+    double value = matrix_determinant(matrix);                               \
+    if(     (fabs(1.005 * value) >= fabs(expected))                          \
+         && (fabs(0.995 * value) <= fabs(expected))                          \
+         && ((expected * value) >= 0))                                       \
+    {                                                                        \
+        cout <<"Everything is fine! Thank you!\n"<<endl;                                                 \
+    }                                                                        \
+    else                                                                     \
+    {                                                                        \
+        cout<<"FAIL!" << endl << "Expected: "<< expected << ", got: "<< value << endl;                  \
+        Passed=1;                                                \
+    }                                                                             \
+   cout << endl;                                                            \
+}
+
+
+int main() {
+    int Passed=0;
+    vector<double> Test1data = {6.8};
+    Matrix Test1(1, 1, Test1data);
+    TEST_EQUAL(Test1, 6.8);
+
+
+    vector<double> Test2data = {1.8,5.6,
+                                112.5,909.86};
+    Matrix Test2 (2, 2, Test2data);
+    TEST_EQUAL(Test2, 1007.748);
+
+
+    vector<double> Test3data = {8.2,	45.1,	56.57,
+                               67.5,	24.22,	5.3,
+                              77.6,	54.4,	9.9};
+    Matrix Test3 (3, 3, Test3data);
+    TEST_EQUAL(Test3,89415.918);
+
+
+    vector<double> Test4data= {4.22,	-5.56,	8.8,	-5.13,
+                              8.2,	4.35	,-8.266,	5.4,
+                              4.5	,-6.447	,-6.2,	1.22,
+                               -9.65,	5.26	,5.56,	3.65};
+    Matrix Test4 (4, 4, Test4data);
+    TEST_EQUAL(Test4, -5545.3962);
+
+
+    vector<double> Test5data= {
+            3,	0	,0,	0,	3,
+            0,	5,	0,	2,	0,
+            0,	0,	7,	0,	5,
+            0,	3,	0,	7,	0,
+            4, 0,	0,	0,	9};
+    Matrix Test5 (5, 5, Test5data);
+    TEST_EQUAL(Test5, 3045);
+
+if(Passed==1)
+{
+    return 1;
+}
+else {
+    return 0;
+}
+}
---
+++ VladBurin/Matrix/matrix.h
@@ -0,0 +1,91 @@
+//
+// Created by vladburin on 30.11.18.
+//
+
+#ifndef VLADBURIN_MATRIX_H
+#define VLADBURIN_MATRIX_H
+#include <vector>
+using namespace std;
+
+
+class Matrix
+{
+public:
+
+    Matrix(unsigned int Valrows, unsigned int Valcols, vector<double> &Valdata)
+    {
+        rows=Valrows;
+        cols=Valcols;
+        data=Valdata;
+        Valdata.clear();
+    }
+
+    unsigned int rows;
+    unsigned int cols;
+    vector<double> data;
+};
+
+
+
+// След матрицы
+extern double matrix_trace(const Matrix A); //
+
+// Определитель матрицы
+extern double matrix_determinant(const Matrix A); //
+
+// Собственные числа матрицы
+
+extern double matrix_eigen_values(const Matrix A);
+
+
+// Матрица собственных векторов
+extern Matrix matrix_eigen_vectors(const Matrix A);
+
+
+// Сумма матриц
+extern Matrix matrix_sum(const Matrix A, const Matrix B); //
+
+// Умножение матриц
+extern Matrix matrix_mult(const Matrix A, const Matrix B); //
+
+// Умножение матрицы на скаляр
+extern Matrix matrix_mult__scalar(const double scalar, const Matrix A); //
+
+// Транспонирование матрицы
+extern Matrix matrix_trans(const Matrix A); //
+
+// Обращение матрицы
+extern Matrix matrix_invert(const Matrix A);
+
+// Матричная экспонента
+extern Matrix matrix_exp(const Matrix A);
+
+// Возведение матрицы в степень (натуральное число или 0)
+extern Matrix matrix_power(const Matrix A, const unsigned int power);
+
+
+// Единичная матрица
+extern Matrix matrix_one(const unsigned int rows, const unsigned int cols); //
+
+// Нулевая матрица
+extern Matrix matrix_zero(const unsigned int rows, const unsigned int cols); //
+
+// Матрица оставленная из случайных чисел из диапазона [-1, +1]
+extern Matrix matrix_rand(const unsigned int rows, const unsigned int cols);
+
+
+// Вывести матрицу на экран
+extern void matrix_print(const Matrix A);
+
+
+// ТАУ
+
+// Решение уравнения Ляпунова X * A + A^T * X = -C
+extern Matrix matrix_lyapunov_equation(const Matrix A, const Matrix C);
+
+
+
+
+#endif //VLADBURIN_MATRIX_H
+
+
