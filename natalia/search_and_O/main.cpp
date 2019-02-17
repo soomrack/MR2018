ce1f8219b6c14bd2a0399d0bc192f342c9bcb78c
Natalia <natalia_maltseva@bk.ru>
2019-02-17 19:00:50 +0300

    linsearch and binsearch

--- natalia/search_and_O/main.cpp
+++ natalia/search_and_O/main.cpp
@@ -1,39 +1,26 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-
-void swap(int *arr, int first_index, int second_index) {
-    int temp = arr[ first_index ];
-    arr[ first_index ] = arr[ second_index ];
-    arr[ second_index ] = temp;
-}
-
-void bubbleSort(int *arr, int m){
-    for(int i = m - 1; i >= 1; i--)
-        for(int j = 0; j < i; j++)
-        {
-            if(arr[j] > arr[j+1])
-            {
-               swap(arr, j, j+1);
-            }
-        }
-
-
-}
+#include "search_and_O.h"
 
 int main() {
     int n = 5;
     int *array;
     array = new int [n];
-    srand(time(0)); //текущее системное время
+   // srand(time(0)); //текущее системное время
     for (int i = 0; i < n; i++)
     {
-        array[i] = rand()%100;
+        array[i] = n-i;
+        //array[i] = rand()%100;
         std::cout << array[i] << ' ';
     }
+
     bubbleSort(array,n);
     std::cout << std::endl;
     for(int i = 0; i < n; i++)
         std::cout << array[i] << ' ';
+    std::cout << std::endl;
+    std::cout << linsearch(array, n, 4)<< std::endl;
+    std::cout << binsearch(array, 0, n, 4);
     return 0;
 }
---
+++ natalia/search_and_O/search_and_O.h
@@ -0,0 +1,12 @@
+
+#ifndef SEARCH_AND_O_SEARCH_AND_O_H
+#define SEARCH_AND_O_SEARCH_AND_O_H
+
+
+void swap(int *arr, int first_index, int second_index);
+void bubbleSort(int *arr, int m);
+int linsearch (int *arr, int n, int x);
+int binsearch ( int *arr, int begin, int n, int x );
+
+
+#endif //SEARCH_AND_O_SEARCH_AND_O_H
