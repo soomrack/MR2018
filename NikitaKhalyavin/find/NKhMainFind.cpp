64c6d863df3ca14e0125925b75b68bab1ad7d73b
KhalyavinNikita <nikitahalyavin5@gmail.com>
2019-02-18 12:31:25 +0300

    Change, mergeSort

--- NikitaKhalyavin/find/NKhMainFind.cpp
+++ NikitaKhalyavin/find/NKhMainFind.cpp
@@ -7,13 +7,22 @@
 #include <stdio.h>
 
 int main() {
-    int a[50];
-    for(int i = 0; i < 50; i++){
-        a[i] = 40 - i;
-    }
-    int out = findForNotSorted(a, 30, 50);
-    printf("%d\t", out);
-    BubbleSort(a, 50);
-    out = findForSorted(a, 30, 50, 0, 49);
+    int * a = CreateRandArray(50, -100, 100);
+    ArrayPrint(a, 50);
+    MergeSort(a, 50);
+    ArrayPrint(a, 50);
+
+    int out = findForSorted(a, 30, 50, 0, 49);
     printf("%d\n", out);
+
+    change(a,25,30,50);
+
+    ArrayPrint(a, 50);
+    /*
+    int out = findForNotSorted(a, 30, 50);
+    printf("%d\n", out);
+    BubbleSort(a, 50);
+    ArrayPrint(a, 50);
+    out = findForSorted(a, 30, 50, 0, 49);
+    printf("%d\n", out);*/
 }
