8fda3a520dcad02b79d42eaad20c2c68e22fb548
KhalyavinNikita <nikitahalyavin5@gmail.com>
2019-03-25 14:18:37 +0300

    Наконец работает всё в бинарном дереве

--- NikitaKhalyavin/find/NKhMainFind.cpp
+++ NikitaKhalyavin/find/NKhMainFind.cpp
@@ -6,8 +6,10 @@
 #include "ArraySort.h"
 #include "trees.h"
 #include <stdio.h>
+#include <iostream>
 
 
+using namespace std;
 
 void dataListPrint(List<int> data) {
     for(unsigned int i = 0; i < data.getSize(); i++) {
@@ -16,6 +18,8 @@ void dataListPrint(List<int> data) {
     }
     printf("\n");
 }
+
+
 
 int main() {
 
@@ -32,7 +36,31 @@ int main() {
     tree1.deleteItemWithoutChildren(512);
 
     List<int> temp;
-    tree1.fastSort();
-    //temp = tree1.visiting1();
+
+
+    temp = tree1.visiting1();
+
+    for (int i = 0; i < temp.getSize(); i++) {
+        printf("%d\t", temp.getItem(i));
+    }
+
+    tree1.balancing();
+
+    temp = tree1.visiting1();
+
+    for (int i = 0; i < temp.getSize(); i++) {
+        printf("%d\t", temp.getItem(i));
+    }
+        /*
+    Stack<int> st1;
+    st1.push(1);
+    st1.push(2);
+    st1.push(3);
+    Stack<int> st2(st1);
+    printf("%d\t", st2.pop());
+    st1 = st2;
+    printf("%d\t", st1.pop());
+    printf("%d\t", st1.pop());
+    printf("%d\t", st1.pop());*/
 
 }
